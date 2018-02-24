170f1246b7a107600ce6cb5b057482509574e286
SobolevYuri <yura.sobolev.91@gmail.com>
2018-02-24 12:36:25 +0300

    My 7th task (Zeros)

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 module.exports = function getZerosCount(number) {
-  // your implementation
+let num=number,div2=0,div5=0,current;
+
+for (let i=1;i<=num;i++)
+{
+  current=i;
+
+while(current%2==0) {
+  current=Math.floor(current/2);
+  div2++;
+}
+while(current%5==0){
+  current=Math.floor(current/5);
+  div5++;
+}
+}
+if(div2>div5){
+  return div5;
+}
+return div2;
 }
