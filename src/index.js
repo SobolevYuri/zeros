83b62a6922b90f3c2d6f5c667b4ba3301ae0c843
SobolevYuri <yura.sobolev.91@gmail.com>
2018-02-25 17:43:17 +0300

    My 7th task with Zeros

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 module.exports = function getZerosCount(number) { 
-  let current= number, zerCount = 0; 
-  while(current>=1){ 
+
+      var current= number, zeroNumber = 0; 
+
+   while(current>=1){ 
     current=Math.floor(current/5); 
-    zerCount+=current; 
-  } 
-  return zerCount; 
+    zeroNumber+=current; 
+       } 
+         return zeroNumber; 
   };
