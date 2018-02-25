e556643c28b0e639360411475fceffb69ab8bb4e
SobolevYuri <yura.sobolev.91@gmail.com>
2018-02-25 10:52:38 +0300

    My 7-th tqsk (Zeros)

--- src/index.js
+++ src/index.js
@@ -1,21 +1,8 @@
-module.exports = function getZerosCount(number) {
-let num=number,div2=0,div5=0,current;
-
-for (let i=1;i<=num;i++)
-{
-  current=i;
-
-while(current%2==0) {
-  current=Math.floor(current/2);
-  div2++;
-}
-while(current%5==0){
-  current=Math.floor(current/5);
-  div5++;
-}
-}
-if(div2>div5){
-  return div5;
-}
-return div2;
-}
+module.exports = function getZerosCount(number) { 
+  let current= number, zerCount = 0; 
+  while(current>=1){ 
+    current=Math.floor(current/5); 
+    zerCount+=current; 
+  } 
+  return zerCount; 
+  };
